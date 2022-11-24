f87b020f9e8f5df25c1875de8a3e05ccf273aea7
S Murad <118879062+ShaikMurad@users.noreply.github.com>
2022-11-24 18:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+// // var obj = {
+// //  name: "john",
+// //  age :  23
+
+// // }
+// // obj.gender = "male";
+
+// // console.log(obj.name);
+
+// // Array of objects:
+
+// var arr = [
+// {
+//     "name"  :   "john",
+//     "age"   :   "20"
+// },
+// {
+//     "name"  :   "doe",
+//     "age"   :   "21"
+
+// },
+// {
+//     "name"  :   "Murad",
+//     "age"   :   "22" 
+// }
+
+
+// ];
+// for (var i=0; i<arr.length; i++)
+// {
+//     console.log(arr[i].name,arr[i].age);
+// }
+
+var resume ={
+
+    "name": "S Murad",
+    "father_name" : "SJ Zikriya",
+    "mother_name" : "S J Chotima",
+    "age" : 25,
+    "gender":"male",
+    "qualification":["SSC", "Inter","B.Com(Computer Application)"],
+    "location" : "Chittoor",
+    "experience" : "5 years experience in Title",
+    "languages_known":["Tamil","Telugu","English", "Hindi"]
+
+
+}
+console.log(resume);
+
+// var obj = {
+//     "name": "john",
+//     "age" : "20",
+//     "gender":"male",
+//     "attendance":["mon","tue","wed","thu"]
+
+// }
+// for (var i=0; i<obj.attendance.length; i++){
+//     console.log(obj.attendance[i]);
+// }
+
+// var obj = {
+//     "name": "john",
+//     "age" : "20",
+//     "gender":"male",
+//     "attendance":["mon","tue","wed","thu"]
+    
+//     for(var i in obj.attendance) {
+//     console.log(i, obj.attendance[i]);
+// }
+
+// var obj = {
+// "name": "John",
+// "age" : "20",
+// "gender":"male",
+// "attendance":["mon","tue","wed","thu"]
+// }
+// for(var i in obj.attendance) {
+//     console.log(i, obj.attendance[i]);
+// }
+
+
+
+
+// var obj = {
+//     "name": "john",
+//     "age" : "20",
+//     "gender":"male",
+//     "attendance":["mon","tue","wed","thu"]
+    
+// }
+// console.log(obj);
+    // for (var i=0; i<obj.attendance.length; i++){
+    //         console.log(obj.attendance[i]);
+    // }
+
+ 
+//     var arr = [
+        
+
+// {
+//     "name":"john",
+//     "age":"20"
+// },
+
+// {
+//     "name": "doe",
+//     "age": "21"
+// },
+
+// {
+//     "name": "Murad",
+//     "age": "22"
+// },
+
+// {
+//     "name":"gulam",
+//     "age": "24"
+// }
+// ];
+// for(var i=0; i<arr.length; i++)
+//     console.log(arr[i].name,arr[i].age);
+
+// var names = ["jerry", "tom", "pluto", "micky", "mini"];
+// names.forEach(function1);
+// function function1(currentValue, index) {
+//    console.log("Index in array is: "+index + " ::  Value is: "+currentValue);
+// }
+
+// var names = ["jerry", "tom", "pluto", "micky", "mini"];
+// names.forEach(function1);
+// function function1(currentValue, index){
+//     console.log("Index in array is: "+index + " :: value is: "+currentValue);
+// }
